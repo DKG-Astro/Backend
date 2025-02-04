c0ed03fad656976ee19971d183e78cb07b039d0a
abkumar92 <abhishekk334@gmail.com>
2025-02-04 11:59:42 +0530

    workflow related chnage

--- src/main/java/com/astro/repository/UserMasterRepository.java
+++ src/main/java/com/astro/repository/UserMasterRepository.java
@@ -4,7 +4,11 @@ import com.astro.entity.UserMaster;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserMasterRepository extends JpaRepository<UserMaster, Integer> {
     UserMaster findByUserIdAndPassword(Integer userId, String password);
+
+    Optional<UserMaster> findByCreatedBy(String createdBy);
 }
